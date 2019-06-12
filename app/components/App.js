d168c2dd044a63dedc3c43a037f00de96b5c705b
Cody Goss <c.ward.goss@gmail.com>
2019-06-12 11:42:41 -0400

    edits to the paragraph and lite refactors

--- app/components/App.js
+++ app/components/App.js
@@ -92,7 +92,7 @@ class App extends React.Component {
               value: 0,
               endValue: 3.7259,
               borderColor: 'rgb(75, 192, 192)',
-              borderWidth: 4,
+              borderWidth: 1,
               label: {
                 enabled: false,
                 backgroundColor: 'rgba(0, 0, 0, 0.0)',
@@ -112,24 +112,36 @@ class App extends React.Component {
         <div className='jumbotron'>
           <h1 className='display-4'>Cody Goss GPA analysis</h1>
           <p className='lead'>
-            GPA is a common factor for judging wether or not a student will do
+            GPA is a common factor for judging whether or not a student will do
             well within a university level program. Most students who have
             higher GPA have great study habits, are good test takers, and are
-            able to prioritize thier time adequetly.
+            able to prioritize their time adequately.
           </p>
           <hr className='my-4' />
           <p>
-            The chart below shows the realationship between my semeter GPA and
-            time. The trendline has a postive assoiciation that shows as time
-            increases GPA steadily rises.
+            Early in my college career I didn’t know how to study, I was not
+            focused, and I didn’t understand how to prepare for test. As time
+            went on I learned the hard way how to do these things. The trendline
+            below is a easy visualization of how as I grew as a student and as a
+            person my GPA steadily increased. I learned how to prepare for test,
+            I learn how I learn and how to study, and I learned how to
+            prioritize work and school.{' '}
           </p>
-          <a className='btn btn-primary btn-lg' href='#' role='button'>
+          <a
+            className='btn btn-primary btn-lg'
+            target='_blank'
+            href='https://www.linkedin.com/in/mudanic/'
+            role='button'
+          >
             Learn more
           </a>
         </div>
         <div className='row'>
           <div className='col d-flex justify-content-center'>
             <canvas id='myChart' />
+            <p className='p-2 mt-4'>
+              This chart shows my end of semester GPA plotted against time
+            </p>
           </div>
         </div>
       </div>
